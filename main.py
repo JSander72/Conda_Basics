298ca620ae96ef81dd2f50f4b774b39201ceefa9
JSander72 <jamesearlsanders2@outlook.com>
2025-05-18 11:51:44 -0400

    Add initial Python script and VSCode settings

---
+++ main.py
@@ -0,0 +1,17 @@
+#name = 'James'
+# first_name = 'James'
+# last_name = 'Smith'
+# print(f'Hello, {first_name + last_name}') 
+# This is a simple Python script that prints a greeting message.
+# The script uses an f-string to format the message, which is a feature introduced in Python 3.6.  
+# F-strings are a way to embed expressions inside string literals, using curly braces {}.
+
+#type castinig = covert the datea type of a valuse to another data type 
+
+x = 1   #int
+y = 2.0 #float
+z = "3" #string
+
+print(x)
+print(y)
+print(z)
